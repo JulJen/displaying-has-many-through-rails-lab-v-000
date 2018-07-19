7275a43f756c3a586dc6a6e8e6361a87d5a33bea
Jul Jen <juliajen.prog@gmail.com>
2018-07-19 17:31:56 +0000

    automatically backed up by learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,8 +2,15 @@ class Appointment < ActiveRecord::Base
   belongs_to :patient
   belongs_to :doctor
 
+<<<<<<< HEAD
   def datetime_format
      self.appointment_datetime.strftime("%B %e, %Y at %k:%M")
    end
 
+=======
+  def datetime_to_readable
+     self.appointment_datetime.strftime("%B %e, %Y at %k:%M")
+   end
+   
+>>>>>>> 91899b41162344e2b926e45087799c3e2e0c15fd
 end
